8a31d38d5bf868a92491af43a74128c309f915ad
Elliot Saba <staticfloat@gmail.com>
2023-01-27 18:24:47 +0000

    Set stage1 to generate a read-only root filesystem
    We mount a few tmpfs locations here and there, but in general we're
    read-only except for `/data`.
    
    We also automatically grow `/data` on every boot via the newly-added
    `grow_data_partition` tool in `/usr/lib/`
    
    Add `qemu_customize` step
    
    This step allows us to customize a base image into multiple, specialized
    images using `qemu` locally.

---
+++ stage1/04-prepare-readonly-root/00-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Create the mountpoints, also check the `fstab` entry that creates a tmpfs
+mkdir -p "${ROOTFS_DIR}/var/lib/systemd/timesync"
+mkdir -p "${ROOTFS_DIR}/var/lib/systemd-logind"
+
+# Also install our `ro` and `rw` scripts
+install -m 755 files/ro "${ROOTFS_DIR}/usr/local/bin/ro"
+install -m 755 files/rw "${ROOTFS_DIR}/usr/local/bin/rw"
+
+# Install some scripts we run during startup
+install -m 755 files/grow_data_partition "${ROOTFS_DIR}/usr/lib/grow_data_partition"
+install -m 755 files/regenerate_ssh_host_keys "${ROOTFS_DIR}/usr/lib/regenerate_ssh_host_keys"
+install -m 755 files/qemu_customize "${ROOTFS_DIR}/usr/lib/qemu_customize"
